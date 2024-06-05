d6dfb88482abcb5cf773ca142546eb7b3d5c059b
Josef Antoni <antoni.josef@icloud.com>
2024-06-05 16:43:37 +0200

    Project creation

---
+++ Kalorie/Kalorie/KalorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalorieApp.swift
+//  Kalorie
+//
+//  Created by Josef Antoni on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct KalorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
