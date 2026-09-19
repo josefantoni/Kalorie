d96bd1fb56796fcde33e1eb0a99e7aa697a544d2
Josef Antoni <antoni.josef@icloud.com>
2026-09-19 18:07:01 +0200

    [REFACTOR]: build all KMP modules with Kotlin 2.4.10 instead of 2.1.0
    Kotlin/Native 2.1.0 refuses klibs built with a newer compiler, which blocked
    PdfKmp for the data export. Recorded in ADR 0034.

--- TextKit/build.gradle.kts
+++ TextKit/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
-    kotlin("multiplatform") version "2.1.0"
+    kotlin("multiplatform") version "2.4.10"
 }
 
 repositories {
